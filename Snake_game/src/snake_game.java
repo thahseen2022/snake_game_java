27bed935600875e8f08b0086e924ac96f612ffed
Thahseen <116421550+thahseen2022@users.noreply.github.com>
2023-03-08 04:33:11 +0530

    Add files via upload

---
+++ Snake_game/src/snake_game.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author thahs
+ *
+ */
+public class snake_game {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new GameFrame();
+
+	}
+
+}
